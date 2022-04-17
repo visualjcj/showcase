8849241032190caf07ff520660d9fa351d6f756d
josteda99 <josteda99@users.noreply.github.com>
2022-04-17 20:09:44 +0000

    deploy: 72c506bf695e447eb751d9286d424b66e03d18cb

--- sketches/ejercicios/sketch.js
+++ sketches/ejercicios/sketch.js
@@ -12,7 +12,7 @@ var offsetX = 0,
 var colorGradient = []
 
 function setup() {
-  let c = createCanvas(500, 500, WEBGL);
+  let c = createCanvas(500, 500, WEBGL)
   // saveCanvas(c)
   back = loadImage('../../../sketches/ejercicios/assets/gradient.png')
   sol = loadImage('../../../sketches/ejercicios/assets/sun.png')
@@ -33,10 +33,11 @@ function setup() {
 function draw() {
   //background
   background(200)
-  image(back, -800, -1000, 2000, 1500);
+  image(back, -800, -1000, 2000, 1500)
+
   //fov
   let fov = PI / 1.5
-  let cameraZ = (height / 2) / tan(fov / 2)
+  let cameraZ = height / 2 / tan(fov / 2)
   perspective(fov, width / height, cameraZ / 10, cameraZ * 10)
 
   //terrain
@@ -49,25 +50,60 @@ function draw() {
   for (let i = 0; i < cols; i++) {
     offsetY = 0
     for (let j = 0; j < rows; ++j) {
-      if (j >= (rows / 2) - 15 && j <= (rows / 2) - 5) {
-        terrain[(i * rows) + j] = map(noise(offsetY, offsetX), 0, 1, -bound * 0.1, bound * 0.1)
+      if (j >= rows / 2 - 15 && j <= rows / 2 - 5) {
+        terrain[i * rows + j] = map(
+          noise(offsetY, offsetX),
+          0,
+          1,
+          -bound * 0.1,
+          bound * 0.1,
+        )
       } else {
-        terrain[(i * rows) + j] = map(noise(offsetY, offsetX), 0, 1, -bound / 2 + 10, bound)
+        terrain[i * rows + j] = map(
+          noise(offsetY, offsetX),
+          0,
+          1,
+          -bound / 2 + 10,
+          bound,
+        )
       }
       offsetY += 0.16
     }
     offsetX += 0.1
   }
 
-  for (let y = 0; y < cols; y++) {
+  for (let y = 0; y < cols - 1; y++) {
     beginShape(TRIANGLE_STRIP)
     for (let x = 0; x < rows; x++) {
-      if (x >= (rows / 2) - 15 && x <= (rows / 2) - 5) {
-        fill(colorGradient[floor(map(terrain[y * rows + x], -bound * 0.1, bound * 0.1, 0, 1))])
+      if (x >= rows / 2 - 15 && x <= rows / 2 - 5) {
+        fill(
+          colorGradient[
+            floor(map(terrain[y * rows + x], -bound * 0.1, bound * 0.1, 0, 1))
+          ],
+        )
       } else {
-        fill(colorGradient[floor(map(terrain[y * rows + x], -bound / 2 + 10, bound, 0, 4))])
+        fill(
+          colorGradient[
+            floor(map(terrain[y * rows + x], -bound / 2 + 10, bound, 0, 4))
+          ],
+        )
       }
       vertex(x * scl, y * scl, terrain[x + y * rows])
+      if (x >= rows / 2 - 15 && x <= rows / 2 - 5) {
+        fill(
+          colorGradient[
+            floor(
+              map(terrain[x + (y + 1) * rows], -bound * 0.1, bound * 0.1, 0, 2),
+            )
+          ],
+        )
+      } else {
+        fill(
+          colorGradient[
+            floor(map(terrain[x + (y + 1) * rows], -bound / 2, bound, 0, 2))
+          ],
+        )
+      }
       vertex(x * scl, (y + 1) * scl, terrain[x + (y + 1) * rows])
     }
     endShape()
