9689cab31ee0397b95c739e40f0b2c75cdace849
josteda99 <josteda99@users.noreply.github.com>
2022-06-20 21:56:34 +0000

    deploy: 332cf0258188821b3f622b07890359f92440e772

---
+++ mask_001.frag
@@ -0,0 +1,64 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform sampler2D texture;
+uniform vec2 texOffset;
+// holds the 3x3 kernel
+uniform float mask[9];
+uniform vec2 u_mouse;
+uniform vec2 u_resolution;
+uniform int option;
+// we need our interpolated tex coord
+varying vec2 texcoords2;
+
+void main() {
+
+  //0 Mask - 1 magnifier
+  if(option == 0) {
+  // 1. Use offset to move along texture space.
+  // In this case to find the texcoords of the texel neighbours.
+    vec2 tc0 = texcoords2 + vec2(-texOffset.s, -texOffset.t);
+    vec2 tc1 = texcoords2 + vec2(0.0, -texOffset.t);
+    vec2 tc2 = texcoords2 + vec2(texOffset.s, -texOffset.t);
+    vec2 tc3 = texcoords2 + vec2(-texOffset.s, 0.0);
+  // origin (current fragment texcoords)
+    vec2 tc4 = texcoords2 + vec2(0.0, 0.0);
+    vec2 tc5 = texcoords2 + vec2(texOffset.s, 0.0);
+    vec2 tc6 = texcoords2 + vec2(-texOffset.s, texOffset.t);
+    vec2 tc7 = texcoords2 + vec2(0.0, texOffset.t);
+    vec2 tc8 = texcoords2 + vec2(texOffset.s, texOffset.t);
+
+  // 2. Sample texel neighbours within the rgba array
+    vec4 rgba[9];
+    rgba[0] = texture2D(texture, tc0);
+    rgba[1] = texture2D(texture, tc1);
+    rgba[2] = texture2D(texture, tc2);
+    rgba[3] = texture2D(texture, tc3);
+    rgba[4] = texture2D(texture, tc4);
+    rgba[5] = texture2D(texture, tc5);
+    rgba[6] = texture2D(texture, tc6);
+    rgba[7] = texture2D(texture, tc7);
+    rgba[8] = texture2D(texture, tc8);
+
+  // 3. Apply convolution kernel
+    vec4 convolution;
+    for(int i = 0; i < 9; i++) {
+      convolution += rgba[i] * mask[i];
+    }
+    gl_FragColor = vec4(convolution.rgb, 1.0);
+
+  } else if(option == 1) {
+
+    float R = 100.;
+    float h = 40.;
+    float hr = R * sqrt(1. - ((R - h) / R) * ((R - h) / R));
+
+    vec2 xy = gl_FragCoord.xy - u_mouse.xy;
+    float r = sqrt(xy.x * xy.x + xy.y * xy.y);
+    //vec2 new_xy = r > hr ? xy * (R - h) / sqrt(R * R - r * r) : xy;
+    vec2 new_xy = r < hr ? xy * (R - h) / sqrt(R * R - r * r) : xy;
+
+    gl_FragColor = texture2D(texture, (new_xy + u_mouse.xy) / u_resolution);
+  }
+}
---
+++ sketches/entrega3julio/sketch.js
@@ -0,0 +1,138 @@
+let maskShader;
+let img;
+let video_src;
+let video_on;
+let mask;
+/* 
+	References:
+	https://docs.isf.video/primer_chapter_6.html#what-is-a-convolution-matrix 
+	https://programmathically.com/understanding-convolutional-filters-and-convolutional-kernels/ 
+*/
+
+function preload() {
+  maskShader = readShader("/showcase/mask_001.frag", {
+    varyings: Tree.texcoords2,
+  });
+  img = loadImage("/showcase/flower.png");
+}
+
+function setup() {
+  // shaders require WEBGL mode to work
+  createCanvas(650, 500, WEBGL);
+  noStroke();
+  textureMode(NORMAL);
+
+  video_on = createCheckbox("Video", false);
+  video_on.style("color", "white");
+  video_on.changed(() => {
+    if (video_on.checked()) {
+      maskShader.setUniform("texture", video_src);
+      video_src.loop();
+      video_src.volume(0.0);
+    } else {
+      maskShader.setUniform("texture", img);
+      video_src.pause();
+    }
+  });
+  video_on.position(100, 10);
+  mask = createSelect();
+  mask.option("None", 0);
+  mask.option("Gaussian", 1);
+  mask.option("Box Blur", 2);
+  mask.option("Laplacian", 3);
+  mask.option("Edge", 4);
+  mask.option("Emboss", 5);
+  mask.option("Sharpen", 6);
+  mask.option("Sobel", 7);
+  mask.option("Magnifier", 8);
+  mask.position(10, 10);
+  mask.changed(() => {
+    switch (mask.value()) {
+      case "0":
+        maskShader.setUniform(
+          "mask",
+          [0.0, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 0.0]
+        ); // Identity
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "1":
+        maskShader.setUniform(
+          "mask",
+          [0.0625, 0.125, 0.0625, 0.125, 0.25, 0.125, 0.0625, 0.125, 0.0625]
+        ); // Gaussian blur
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "2":
+        maskShader.setUniform(
+          "mask",
+          [
+            0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111, 0.1111,
+            0.1111,
+          ]
+        ); // Box blur
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "3":
+        maskShader.setUniform(
+          "mask",
+          [1.0, 0.0, 1.0, 0.0, -4.0, 0.0, 1.0, 0.0, 1.0]
+        ); // Laplacian
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "4":
+        maskShader.setUniform(
+          "mask",
+          [-1.0, -1.0, -1.0, -1.0, 8.0, -1.0, -1.0, -1.0, -1.0]
+        ); // Edge detect
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "5":
+        maskShader.setUniform(
+          "mask",
+          [-2.0, -1.0, 0.0, -1.0, 1.0, 1.0, 0.0, 1.0, 2.0]
+        ); // Emboss
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "6":
+        maskShader.setUniform(
+          "mask",
+          [-1.0, 0.0, -1.0, 0.0, 5.0, 0.0, -1.0, 0.0, -1.0]
+        ); // Sharpen
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "7":
+        maskShader.setUniform(
+          "mask",
+          [1.0, 0.0, -1.0, 2.0, 0.0, -2.0, 1.0, 0.0, -1.0]
+        ); // Sobel
+        maskShader.setUniform("option", 0); // Magnifier
+        break;
+      case "8":
+        maskShader.setUniform("option", 1); // Magnifier
+        break;
+      default:
+        console.log(mask.value());
+        break;
+    }
+  });
+  shader(maskShader);
+  maskShader.setUniform("texture", img);
+  maskShader.setUniform("mask", [0.0, 0.0, 0.0, 0.0, 1, 0.0, 0.0, 0.0, 0.0]); // Identity
+  emitTexOffset(maskShader, img, "texOffset");
+  emitResolution(maskShader, "u_resolution");
+}
+
+function draw() {
+  emitMousePosition(maskShader, "u_mouse");
+  background(0);
+  quad(
+    -width / 2,
+    -height / 2,
+    width / 2,
+    -height / 2,
+    width / 2,
+    height / 2,
+    -width / 2,
+    height / 2
+  );
+}
