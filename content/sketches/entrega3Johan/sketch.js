ca1ae3044ad03a12eee92dcfc11a3add148b5109
josteda99 <josteda99@gmail.com>
2022-06-20 10:12:11 -0500

    add controls

--- content/sketches/entrega3Johan/sketch.js
+++ content/sketches/entrega3Johan/sketch.js
@@ -3,7 +3,7 @@ let pg;
 let horse;
 
 function preload() {
-  shaderu = loadShader("/showcase/basic.vert", "/showcase/3_truchet.frag");
+  shaderu = loadShader("/showcase/basic.vert", "/showcase/all_truchet.frag");
   horse = loadModel("/showcase/horse.obj", true);
 }
 
@@ -18,14 +18,35 @@ function setup() {
     width * pixelDensity(),
     height * pixelDensity(),
   ]);
+
+  option = createSelect();
+  option.position(10, 10);
+  option.option("option 0", 0);
+  option.option("option 1", 1);
+  option.option("option 2", 2);
+  option.option("option 3", 3);
+  option.option("option 4", 4);
+  option.selected("none");
+  option.changed(() => {
+    shaderu.setUniform("option", option.value());
+  });
+
+  vel = createSlider(0, 1, 0.05, 0.05);
+  vel.position(10, 25);
+  vel.style("width", "280px");
 }
 
 function draw() {
   background(199, 255, 237);
   shaderu.setUniform("u_mouse", [mouseX, mouseY]);
   shaderu.setUniform("u_time", frameCount / 10);
+  shaderu.setUniform("vel", vel.value());
   textuSha.rect(0, 0, width, height);
   texture(textuSha);
+
+  noStroke();
+  quad(80, 80, width, 80, width, height, 80, height);
+
   orbitControl();
   rotateX(20);
   rotateZ(150);
